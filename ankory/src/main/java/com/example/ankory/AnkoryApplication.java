f0e3857799c50b1f78b169c56335b71096b58219
Yoann SEURAT <yoann.seurat@gmail.com>
2026-09-11 11:20:42 +0200

    gradle init

---
+++ ankory/src/main/java/com/example/ankory/AnkoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.ankory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnkoryApplication.class, args);
+	}
+
+}
